ae6f5ff8e427a956bc5689f20c65b8bf31c6dc3f
Prince Thind <Princethind799@gmail.com>
2022-02-04 22:53:58 +0530

    add progress bars

--- src/lib/download.js
+++ src/lib/download.js
@@ -1,47 +1,68 @@
 const fs = require("fs");
 const ytdl = require("ytdl-core");
 const { exec } = require("child_process");
+const cliProgress = require("cli-progress");
+
+const audioBar = new cliProgress.SingleBar({
+  format: "Audio: [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}",
+});
+const videoBar = new cliProgress.SingleBar({
+  format: "Video: [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}",
+});
 
 async function download(url, name) {
+  await clean();
   await downloadAudio(url);
   await downloadVideo(url);
   await merge(name);
   await clean();
 }
 
-async function downloadVideo(url, name = "temp.mp4", path = "./output/") {
-  return new Promise((resolve, reject) => {
-    const stream = ytdl(url, { quality: "highestvideo" }).pipe(
-      fs.createWriteStream(path + name)
-    );
+async function downloadAudio(url, name = "temp.wav", path = "./output/") {
+  audioBar.start(10000, 0);
 
+  return new Promise((resolve, reject) => {
+    const stream = ytdl(url, { quality: "highestaudio" });
+
+    stream.on("progress", (_, current, total) => {
+      trackProgress(audioBar, current, total);
+    });
     stream.on("finish", () => {
       resolve();
+      audioBar.stop();
     });
     stream.on("error", (e) => {
+      console.log(e);
       reject(e);
     });
+    stream.pipe(fs.createWriteStream(path + name));
   });
 }
 
-async function downloadAudio(url, name = "temp.wav", path = "./output/") {
+async function downloadVideo(url, name = "temp.mp4", path = "./output/") {
+  videoBar.start(10000, 0);
+
   return new Promise((resolve, reject) => {
-    const stream = ytdl(url, { quality: "highestaudio" }).pipe(
-      fs.createWriteStream(path + name)
-    );
+    const stream = ytdl(url, { quality: "highestvideo" });
+
+    stream.on("progress", (_, current, total) => {
+      trackProgress(videoBar, current, total);
+    });
 
     stream.on("finish", () => {
       resolve();
+      videoBar.stop();
     });
     stream.on("error", (e) => {
       reject(e);
     });
+    stream.pipe(fs.createWriteStream(path + name));
   });
 }
 
 async function clean() {
-  const command = "rm ./output/temp.mp4 ./output/temp.wav";
-  executeCommand(command);
+  const command = "rm -f ./output/temp.mp4 ./output/temp.wav";
+  await executeCommand(command);
 }
 
 async function merge(name = "video") {
@@ -56,12 +77,16 @@ async function executeCommand(command) {
         console.error(`exec error: ${error}`);
         reject(error);
       }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
+      // console.log(`stdout: ${stdout}`);
+      // console.error(`stderr: ${stderr}`);
       resolve();
     });
   });
 }
 
+function trackProgress(progressBar, current, total) {
+  const percentage = Math.round((current / total) * 10000);
+  progressBar.update(percentage);
+}
 
-module.exports = download
+module.exports = download;
