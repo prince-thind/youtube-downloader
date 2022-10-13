47967ff7809a21ceec2842338c104c1ef6d4bd72
Prince Thind <princethind799@gmail.com>
2022-10-13 23:39:36 +0530

    implement basic downloader

--- preload.js
+++ preload.js
@@ -1,13 +1,19 @@
 const { ipcRenderer } = require('electron');
+const download = require('./lib/download');
 
 window.addEventListener('DOMContentLoaded', () => {
     init()
 })
 
+let progressBar = null;
+let percentageBar = null;
+
 function init() {
     const form = document.getElementById('form');
     const directoryPicker = document.getElementById('dir-picker');
     const dirInput = document.getElementById('dir-input');
+    progressBar = document.getElementById('progress');
+    percentageBar = document.getElementById('percentage');
 
     form.addEventListener('submit', main)
     directoryPicker.addEventListener('click', updateDirectory)
@@ -20,7 +26,12 @@ function init() {
 }
 
 
-
 function main(e) {
     e.preventDefault();
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    const outputPath = formData.get('dir-input');
+
+    download(outputPath, url,{progressBar,percentageBar})
 }
+
