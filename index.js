405a690da29ae3abcf7d885e17e01c94f102da2a
Prince Thind <princethind799@gmail.com>
2022-10-14 17:57:40 +0530

    add form submission js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import "./reload.js";
 import UI from './lib/UI.js';
+import processInput from "./lib/processInput.js";
 
 UI.dirInputHidden.addEventListener('change', pickDir);
 UI.form.addEventListener('submit', main);
 
 function main(e){
     e.preventDefault()
-    console.log(Object.fromEntries(new FormData(e.target)))
+    const res=(Object.fromEntries(new FormData(e.target)));
+    processInput(res);
+
 }
 
 function pickDir(e) {
