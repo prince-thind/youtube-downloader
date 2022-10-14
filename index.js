bc01576d831204f423173eac77963e5775b25977
Prince Thind <princethind799@gmail.com>
2022-10-14 17:28:24 +0530

    implement pickDir function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import "./reload.js";
+import UI from './lib/UI.js';
+
+UI.dirInputHidden.addEventListener('change', pickDir);
+UI.form.addEventListener('submit', main);
+
+function main(e){
+    e.preventDefault()
+    console.log(Object.fromEntries(new FormData(e.target)))
+}
+
+function pickDir(e) {
+    const value = e.target.value;
+    UI.dirInput.value = value;
+}
