be94204bb643e640721f12a0a59a86710b8c86d3
Prince Thind <princethind799@gmail.com>
2022-10-15 22:28:10 +0530

    add frontend post form handling

--- frontend/index.js
+++ frontend/index.js
@@ -2,14 +2,39 @@ const socket = io();
 
 const dirPickerButton = document.querySelector('#dir-label');
 const dirInput = document.querySelector('#dir-input');
+const form = document.querySelector('#form');
+const progress = document.querySelector('#progress');
+const progressLabel = document.querySelector('#progress-label');
+const error = document.querySelector('#error');
+
 dirPickerButton.addEventListener('click', pickDir);
+form.addEventListener('submit', submitForm);
 
 
 socket.on('dir-picked', (dir) => {
-    dirInput.value=dir;
+    dirInput.value = dir;
+})
+
+socket.on('update-progress', ({ progress, message }) => {
+    const percentage = progress.toFixed(2)
+    UI.progress.value = percentage;
+    UI.progressLabel.textContent = `${message}: ${percentage}%`
 })
 
 function pickDir(e) {
     e.preventDefault();
     socket.emit('pick-dir');
+}
+
+function submitForm(e) {
+    e.preventDefault();
+    const formData = Object.fromEntries(new FormData(e.target));
+
+    fetch("/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
 }
--- index.js
+++ index.js
@@ -5,12 +5,15 @@ import initIO from './lib/io/io.js';
 const app = express();
 
 app.use(express.static('frontend'));
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
    res.sendFile(path.resolve('./frontend/index.html'))
 })
 
+app.post('/', (req, res) => {
+   console.log(req.body)
+})
 
 const server = app.listen(3000);
 const io = initIO(server);
