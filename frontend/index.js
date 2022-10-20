8f3d1e626e96da3a09566c449f3e90a0c7d13996
Prince Thind <princethind799@gmail.com>
2022-10-20 16:06:43 +0530

    create basic express server

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,6 @@
-import UI from './lib/UI.js'
+import UI from './lib/UI.js';
+
+const socket=io();
 
 UI.radioButtons.addEventListener('change', UI.toggleIndexInputs);
 UI.showOSPickButton();
---
+++ index.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import express from 'express';
+import http from 'http';
+import {Server} from 'socket.io';
+import IOConnectionHandler from './lib/IO.js';
+
+const app=express();
+const server=http.createServer(app);
+
+const io=new Server(server);
+io.on('connection', IOConnectionHandler);
+
+
+app.use(express.json());
+app.use(express.static(path.resolve("./frontend/")))
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve("./frontend/index.html"));
+})
+
+server.listen(3000);
