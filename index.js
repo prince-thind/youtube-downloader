e354444b2324b62b0131d84cc340b2a193957968
Prince Thind <princethind799@gmail.com>
2022-10-21 12:49:03 +0530

    fix playlist cancel bug

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import path from 'path';
 import express from 'express';
 import http from 'http';
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 import IOConnectionHandler from './lib/IO.js';
 import processInput from './lib/processInput.js';
 
-const app=express();
-const server=http.createServer(app);
+const app = express();
+const server = http.createServer(app);
 
-const io=new Server(server);
+const io = new Server(server);
 io.on('connection', IOConnectionHandler);
 
 
@@ -16,20 +16,22 @@ app.use(express.json());
 app.use(express.static(path.resolve("./frontend/")))
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.sendFile(path.resolve("./frontend/index.html"));
 })
 
-app.post("/",(req,res)=>{
-    processInput(req.body).catch(error=>{
+app.post("/", (req, res) => {
+    processInput(req.body).catch(error => {
         console.error(error);
-        io.emit('error',error.message)
+        io.emit('error', error.message);
+        io.emit('progress', { message: 'Error', showPercentage: false, percentage: 0 })
+
     })
 })
 
 server.listen(3000);
 
-export {io}
+export { io }
 
 
 //todo
